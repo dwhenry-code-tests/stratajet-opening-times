f8425e9443e6096f92df0e4a0bdc0608ecc38eff
David Henry <dw_henry@yahoo.com.au>
2016-01-14 14:09:04 +0000

    Add integration test - not implemented to save time but pseudo code written.

---
+++ spec/features/adding_new_data_file_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'Importing a new data file into the system' do
+  it 'correctly displays the opening time data' do
+
+    pending 'not going to implement this due to simplicity of the system'
+
+    fail
+
+    # Navigate to import screen
+
+    # Import data file
+
+    # Navigate to display screen
+
+    # Check that opening time are displayed
+
+  end
+end
